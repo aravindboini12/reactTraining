6163f12b589f9016d0929d7b53303a4b5cc47dff
Your Name <your_email@example.com>
2025-02-05 12:25:13 +0530

    5/2/25

---
+++ Sidebar1.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Sidebar1(){
+    return(
+        <div className="sidebar1">
+            <h2>sidebar1</h2>
+        </div>
+    )
+}
+export default Sidebar1;
---
+++ Sidebar2.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Sidebar2(){
+    return(
+        <div className="sidebar2">
+            <h2>Sidebar2</h2>
+        </div>
+    )
+}
+export default Sidebar2;
